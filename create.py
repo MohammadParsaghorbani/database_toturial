26e50343e71382046922d11c117d9f1dc58d5b36
MohammadParsaghorbani <mparsagh221@gmail.com>
2024-09-16 07:57:40 +0330

    update

--- create.py
+++ create.py
@@ -1,15 +1,25 @@
 import sqlite3
 
-connection = sqlite3.connect('ilia_data.db')
+connection = sqlite3.connect('data.db')
 cursor = connection.cursor()
 
+# cursor.execute("""
+# create table if not exists price_list(
+#     phone int references users(phone),
+#     book int references book(id),
+#     price int
+# )
+# """)
+
 cursor.execute("""
-create table if not exists price_list(
-    phone int references users(phone),
-    book int references book(id),
-    price int
+create table if not exists users(
+    phone int primary key,
+    name text,
+    last_name text
 )
 """)
+
+
 
 connection.commit()
 connection.close()
